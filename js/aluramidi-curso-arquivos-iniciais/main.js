5efc9ddacf4e3c21a01f8dd3563f0820bd05ded7
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2023-03-12 21:33:20 +0100

    ;
    ;

--- js/aluramidi-curso-arquivos-iniciais/main.js
+++ js/aluramidi-curso-arquivos-iniciais/main.js
@@ -6,7 +6,9 @@ const Lista = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (contador < Lista.length) { //lenght para caso nao saibamos até onde vao os numeros
-    Lista[contador].onclick = toca;
+    Lista[contador].onclick = function() {
+        toca('#som_tecla_pom');
+    }
     contador = contador + 1;
     console.log(contador);
 }
