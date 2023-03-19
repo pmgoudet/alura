c4f1acc515010a3c85f3c433a64a4e960d5dee0b
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2023-03-19 22:39:35 +0100

    .
    .

--- js/aluramidi-curso-arquivos-iniciais/main.js
+++ js/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,13 +1,24 @@
 function toca (idElementoAudio)  {
     document.querySelector(idElementoAudio).play();
 }
+
 const Lista = document.querySelectorAll('.tecla');
+
 for (let contador = 0; contador < Lista.length; contador++) { //lenght para caso nao saibamos até onde vao os numeros
     const tecla = Lista[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`
     //console.log(instrumento)
+    
     Lista[contador].onclick = function() {
         toca(idAudio);
+    }
+
+    tecla.onkeydown = function() { //isso é pra fazer o botão mudar de cor quando acionado pelo "Enter". onkeydown se refere ao teclado.
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 }
