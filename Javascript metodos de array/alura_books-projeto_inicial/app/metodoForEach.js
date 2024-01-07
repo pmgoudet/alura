70e0646e11fcb5cd4b107e9aae6e9852bbd28540
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2024-01-07 12:23:48 +0100

    .
    .

---
+++ Javascript metodos de array/alura_books-projeto_inicial/app/metodoForEach.js
@@ -0,0 +1,18 @@
+const containerLivros = document.getElementById('livros')
+
+function exibirOsLivrosNaTela(listaDeLivros) {
+   listaDeLivros.forEach((livro) => {
+      containerLivros.innerHTML += `<div class="livro">
+         <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+         <h2 class="livro__titulo">
+            ${livro.titulo}
+         </h2>
+         <p class="livro__descricao">${livro.autor}</p>
+         <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+         <div class="tags">
+            <span class="tag">${livro.categoria}</span>
+         </div>
+      </div>`
+   });
+}
+
---
+++ Javascript metodos de array/alura_books-projeto_inicial/app/metodoMap.js
@@ -0,0 +1,29 @@
+function aplicarDesconto (livros) {
+
+   const desconto = 0.3;
+
+   livrosComDesconto = livros.map( livro => {
+      return {... livro, preco: livro.preco - (livro.preco * desconto)}
+   })
+   return livrosComDesconto
+}
+
+
+
+/*
+
+EU FIZ ANTES DA EXPLICAÇÃO
+
+function desconto (porcentagem) {
+   let array = []
+   livros.forEach((livro) => {
+      array.push(livro.preco);
+   })
+
+   const descontoCalculado = array.map((x) => x - x * (porcentagem / 100));
+   let descontoPronto = descontoCalculado.map(x => parseFloat(x.toFixed(2)))
+
+   return descontoPronto;
+}
+*/
+
