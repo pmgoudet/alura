f8c1788db222b3f1bd3fe6dbb9cf9257206dbb7a
pmgoudet <109755812+pmgoudet@users.noreply.github.com>
2024-03-12 19:25:36 +0100

    .
    .

--- Formacao-Desenvolva-aplicações-Web-com-JavaScript/JavaScript validando formularios/monibank-pedro/js/script.js
+++ Formacao-Desenvolva-aplicações-Web-com-JavaScript/JavaScript validando formularios/monibank-pedro/js/script.js
@@ -1,14 +1,21 @@
 const camposFormulario = document.querySelectorAll('[required]');
 
 camposFormulario.forEach((campo) => {
-   campo.addEventListener('blur', () => {
-      console.log('ae')
-   })
-})
+   campo.addEventListener('blur', () => validacaoCampo(campo));
+});
 
+function validacaoCampo(campo) {
+   if (campo.value == "") {
+         console.log('nao valido')
+   } else {
+      console.log('valido')
+   }
+}
 
 
 /*
+
+- NAO LEMBRO COMO  fazer ele validar o campo no blur
 
 - validar CPF
 - data de nascimento tipo data e +18
